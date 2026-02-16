5e4e4afde41d2e4db3372d04391873e1b0612e64
digvijay315 <143862901+digvijay315@users.noreply.github.com>
2026-02-16 10:35:36 +0530

    update

--- vimco-solar-showcase/src/api.tsx
+++ vimco-solar-showcase/src/api.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 const instance=axios.create({
    
-          baseURL:'http://localhost:5000/'
-        //   baseURL:"https://api.intelliviaai.com/"
+          // baseURL:'http://localhost:5000/'
+          baseURL:"https://vimco-solar.onrender.com/"
 
        
 })
